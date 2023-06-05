f9c7caa2f2dc54409a6b32c0924a3a08d6098abd
Vitalii <vetajika@gmail.com>
2023-06-05 18:45:46 +0300

    (fix) requests

--- service/proposal_service.py
+++ service/proposal_service.py
@@ -5,20 +5,26 @@ BASE_URL = 'http://localhost:8080/api/v1'
 
 
 def find_newer_than(last_message_datetime):
-    response = requests.get(url=BASE_URL + '/proposals',
-                            params={
-                                'date': last_message_datetime
-                            })
-    proposals = json.loads(response.content)
+    try:
+        response = requests.get(url=BASE_URL + '/proposals',
+                                params={
+                                    'date': last_message_datetime
+                                })
+        proposals = json.loads(response.content)
+    except:
+        proposals = "[]"
+        pass
+
     return proposals
 
 
 def prettyfi_proposal(proposal):
     string = f"{proposal['title']}\n\n" \
              f"{proposal['price']}\n\n" \
-             f"{proposal['description']}\n\n" \
+             f"{proposal['description']}\n\n"
 
     for tag in proposal['additional_info_tags']:
         string += f"{tag} | "
         pass
+
     return string[:-2]
