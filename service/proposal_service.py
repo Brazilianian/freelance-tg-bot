542c1888afcba25b5af0c9dfc65fb2211eb6d575
Vitalii <vetajika@gmail.com>
2023-06-08 17:43:30 +0300

    (feature) added freelance site

--- service/proposal_service.py
+++ service/proposal_service.py
@@ -15,9 +15,10 @@ def find_newer_than(last_message_datetime):
 
 
 def prettyfi_proposal(proposal):
-    string = f"{proposal['title']}\n\n" \
+    string = f"<strong>{proposal['freelance_site']['name']}</strong>" \
+             f"{proposal['title']}\n\n" \
              f"{proposal['price']}\n\n" \
-             f"{proposal['description']}\n\n"
+             f"{proposal['description']}\n\n" \
 
     for tag in proposal['additional_info_tags']:
         string += f"{tag} | "
